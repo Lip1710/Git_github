a6140a79265bb4ad4109e739d6145a4c614ccd99
Pavel <lotpavel@mail.ru>
2023-01-21 18:53:16 +0300

    Change to Utf-8

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет");
+        System.out.println("РџСЂРёРІРµС‚");
         /*
-         Коммит 1
+         РљРѕРјРјРёС‚ 1
 
 
 
